58de8de4615679c8d43d313d01adb37831e7ef8d
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2022-11-29 14:38:59 -0700

    added

---
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var sentence = "I am learning JavaScript!";
+    sentence += " I have learned a lot from class!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
