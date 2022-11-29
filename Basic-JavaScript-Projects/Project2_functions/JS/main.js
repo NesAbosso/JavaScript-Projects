dc9a690497a3f693ceabe0b834849112627341fd
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2022-11-29 14:41:51 -0700

    commit

--- Basic-JavaScript-Projects/Project2_functions/JS/main.js
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var sentence = "I am learning JavaScript!";
-    sentence += " I have learned a lot from class!";
+    var sentence = "I am learning JavaScript!";  //Created variable
+    sentence += " I have learned a lot from class!"; //a function that uses the += operator
     document.getElementById("Concatenate").innerHTML = sentence;
 }
