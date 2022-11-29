725fea02b65ee39954539c6e148857123b0ff224
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2022-11-29 14:04:08 -0700

    updated

--- Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,11 @@
-var Family = "The Stephens", Dad = "Jarel", Mom = "Cindy",
-    Sister = "Jolie", brother = "Jared";
-document.write(Dad);
+var Sent1 = "This is the beginning of the string" //Created 1st veriable
+var Sent2 = "and this is the end of the string"  //Created 2nd variable
+
+document.write("This is the beginning of the string" //Concatenate the Sent1 and Sent2 variables together
+    + " and this is the end of the string."); //Utilize document.write() method
+
+window.alert("Hello!");  //Utilize the window.alert() method
+
+document.write(4 + 4); // Created an add expression
+
+
