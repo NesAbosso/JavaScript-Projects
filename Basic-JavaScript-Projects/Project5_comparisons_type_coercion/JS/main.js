a8cb5d35fef9153bbd92ae5413e54b34c8d2678a
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2022-11-30 12:22:10 -0700

    _TYPE_COERCION SUBMISSION

---
+++ Basic-JavaScript-Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,25 @@
+document.write(typeof "cat"); //the document.write() method and typeof operator to display the data type of a variable
+
+document.write("cat" + 6); // an expression combining a string and a number.
+
+
+//code utilizing the following operators: ==, ===, >, <, &&, ||, ! 
+
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}
+
+document.write(10 < 2);
+
+document.write(2 > 3);
+
+document.write(10 == 10);
+
+X = 10;
+Y = 10;
+document.write(X === Y);
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(5 > 10 || 10 > 4);
+
