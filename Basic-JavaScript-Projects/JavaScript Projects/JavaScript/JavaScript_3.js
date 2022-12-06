249f502bd01ac7ae0eb3c9bc65213062aebfda4b
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2022-12-06 15:51:58 -0700

    add

---
+++ Basic-JavaScript-Projects/JavaScript Projects/JavaScript/JavaScript_3.js
@@ -0,0 +1,4 @@
+function displayType(character) {
+    var charactorType = character.getAttribute("data-character-type");
+    alert(characterType + " is in the " + character.innerHTML + " universe!");
+}
