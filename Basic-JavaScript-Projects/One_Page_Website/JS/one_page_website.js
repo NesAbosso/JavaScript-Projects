831a435a166945f98b07a696971512b94cf1948e
NesAbosso <117785546+NesAbosso@users.noreply.github.com>
2022-12-08 17:30:18 -0700

    changes

--- Basic-JavaScript-Projects/One_Page_Website/JS/one_page_website.js
+++ Basic-JavaScript-Projects/One_Page_Website/JS/one_page_website.js
@@ -34,3 +34,22 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
     captionText.innerHTML = dots[slideIndex - 1].alt;
 }
+
+
+// Validating Empty Field
+function check_empty() {
+    if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+        alert("Fill All Fields !");
+    } else {
+        document.getElementById('form').submit();
+        alert("Form Submitted Successfully...");
+    }
+}
+//Function To Display Popup
+function div_show() {
+    document.getElementById('abc').style.display = "block";
+}
+//Function to Hide Popup
+function div_hide() {
+    document.getElementById('abc').style.display = "none";
+}
